5733bc3248cba6c06bc58d0492b5f0f22524900e
s.saltykov <s.saltykov@dev.userstory.ru>
2019-02-19 15:59:25 +0700

    class

--- src/class.js
+++ src/class.js
@@ -1,6 +1,34 @@
 import {test, testMap} from 'index';
-class Users {
+
+class Main {
+    constructor(title) {
+        this.title = title;
+        this.done = false;
+    }
+
+    test() {
+        this.done = true;
+        console.log(`${this.title} done`);
+    }
 }
+
+class Task extends Main {
+    constructor(title) {
+        super(title);
+    }
+
+    complete(value){
+        super.test();
+        return value;
+    }
+}
+
+const main = new Main('Пробный класс');
+const task = new Task();
+
+main.test();
+task.test();
+task.complete(22);
 
 test();
 testMap();
